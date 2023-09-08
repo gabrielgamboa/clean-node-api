23e8c1fcfc2f228194454a68e348e93160f45079
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2023-09-08 11:43:54 -0300

    feat: ensure SignUpController calls AddAccount with correct values

---
+++ README.md
@@ -0,0 +1,5 @@
+# PRESENTATION Layer
+Camada de apresentação da aplicação, que recebe a requisição do client
+
+# DOMAIN Layer
+Camada de dominio, core da aplicação
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,13 @@
+import { AddAccount } from '../../domain/usecases/add-account'
 import { InvalidParamError, MissingParamError, ServerError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols'
 
 export class SignUpController implements Controller {
-  constructor (private readonly emailValidator: EmailValidator) {}
+  constructor (
+    private readonly emailValidator: EmailValidator,
+    private readonly addAccount: AddAccount,
+  ) {}
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
@@ -15,15 +19,23 @@ export class SignUpController implements Controller {
         }
       }
 
-      const passwordEqualsToPasswordConfirmation = httpRequest.body.password === httpRequest.body.passwordConfirmation
+      const { name, password, passwordConfirmation, email } = httpRequest.body
+
+      const passwordEqualsToPasswordConfirmation = password === passwordConfirmation
 
       if (!passwordEqualsToPasswordConfirmation) return badRequest(new InvalidParamError('passwordConfirmation'))
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isValid = this.emailValidator.isValid(email)
 
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      this.addAccount.execute({
+        name,
+        email,
+        password
+      })
 
       return {
         statusCode: 400,
