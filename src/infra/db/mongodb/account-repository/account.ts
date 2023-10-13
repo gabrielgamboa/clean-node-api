c1a550678b733ebcd99ab63439765812fba0b5f6
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2023-10-13 10:29:26 -0300

    feat: create add account mongo repository

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,14 @@
+import { AccountRepository } from '../../../../data/protocols/account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const { insertedId } = result
+
+    return Object.assign({}, accountData, { id: insertedId.toString() })
+  }
+}
